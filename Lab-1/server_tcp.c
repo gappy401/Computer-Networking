f7c931e3a658224f5737de5d3792f26629bef05a
gappy401 <nanditaghildyal@gmail.com>
2020-01-22 07:52:22 +0530

    lab1

---
+++ Lab-1/server_tcp.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#define MAXSIZE 90
+
+main()
+{
+int sockfd,newsockfd,retval;
+socklen_t actuallen;
+int recedbytes,sentbytes;
+struct sockaddr_in serveraddr,clientaddr;
+
+char buff[MAXSIZE];
+int a=0;
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+
+if(sockfd==-1)
+{
+printf("\nSocket creation error");
+}
+
+serveraddr.sin_family=AF_INET;
+serveraddr.sin_port=htons(3388);
+serveraddr.sin_addr.s_addr=htons(INADDR_ANY);
+retval=bind(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+if(retval==1)
+{
+printf("Binding error");
+close(sockfd);
+}
+
+retval=listen(sockfd,1);
+if(retval==-1)
+{
+close(sockfd);
+}
+
+actuallen=sizeof(clientaddr);
+newsockfd=accept(sockfd,(struct sockaddr*)&clientaddr,&actuallen);
+
+
+if(newsockfd==-1)
+{
+close(sockfd);
+}
+recedbytes=recv(newsockfd,buff,sizeof(buff),0);
+
+if(recedbytes==-1)
+{
+close(sockfd);
+close(newsockfd);
+}
+int flag=-1;
+    
+   int len= sizeof(buff)-1;
+   for(a=0;a<len/2;a++)
+   {
+       if(buff[a]!=buff[len-a-1])
+       {
+          flag++;
+
+           
+       }
+   }
+   if (flag >=0)
+   {
+       printf("Not Palindrome!");
+       char x[10]= "Not palin";
+       sentbytes=send(newsockfd,x,sizeof(buff),0);
+
+   }
+   else
+   {
+    printf("Palindrme!");
+    char y[10]="Palin";
+    sentbytes=send(newsockfd,y,sizeof(buff),0);
+   }
+   
+
+   
+
+//puts(buff);
+
+//sentbytes=send(newsockfd,buff,sizeof(buff),0);
+
+if(sentbytes==-1)
+{
+close(sockfd);
+close(newsockfd);
+}
+
+close(sockfd);
+close(newsockfd);
+}
